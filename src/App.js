d360c95a44a8a730ca4732d12a79a818ecbb7324
Craig R. Kelly <craig-r-kelly@hotmail.com>
2024-01-04 02:14:17 -0600

    screenshots and a line of

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <div className="App">
-      <header><h1>Delicious Noms: Nom Nom Nom</h1></header>
+      <header><h1>Delicious Food Recipes</h1></header>
       <RecipeList recipes={recipes} onDelete={handleDelete} />
       <RecipeCreate onCreate={handleCreate} />
     </div>
